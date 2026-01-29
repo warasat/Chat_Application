9f1730972d18e98295db6394fdec297ceaee270a
Warasat Ali <warasatwarraich@gmail.com>
2026-01-29 17:14:06 +0500

    resolve voice issue on different device

--- frontend/src/hooks/useAudioCall.ts
+++ frontend/src/hooks/useAudioCall.ts
@@ -90,7 +90,15 @@ export const useAudioCall = ({
 
   const initPeer = async () => {
     const peer = new RTCPeerConnection({
-      iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+      iceServers: [
+        {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+          ],
+        },
+      ],
     });
 
     peer.onicecandidate = (event) => {
